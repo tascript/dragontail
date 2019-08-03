05669ee616e71c35d44924c2867242425c30fcdb
wataru-script <wataruscript8611@gmail.com>
2019-08-04 01:30:52 +0900

    引数のマッチング

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,10 @@ fn main() {
     opts.optopt("c", "color", "change color to string ", "COLOR");
     opts.optopt("w", "word", "highliht the word", "WORD");
     println!("{:?}", args);
+
+    match args.len() {
+        1 => println!("specify target file."),
+        2 => println!("collect args."),
+        _ => println!("wrong args."),
+    }
 }
