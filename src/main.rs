ad748dbf1d711a6d04624300ef93749afece6d13
wataru-script <wataruscript8611@gmail.com>
2019-08-03 22:22:40 +0900

    getoptsの追加

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+extern crate getopts;
+use getopts::Options;
 use std::env;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut opts = Options::new();
+    opts.optopt("c", "color", "change color to string ", "COLOR");
+    opts.optopt("w", "word", "highliht the word", "WORD");
     println!("{:?}", args);
 }
